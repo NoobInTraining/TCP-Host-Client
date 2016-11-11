9b962243d5e7bb7894fcd61c557c96ae7ee86829
Noobi - Miles <ltb350@hotmail.de>
2016-11-11 15:28:42 +0100

    Finished the first try for Host.cs

--- ConsoleApplication1/Host.cs
+++ ConsoleApplication1/Host.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,29 +11,51 @@ namespace ConsoleApplication1
 {
     class Host
     {
+        /// <summary>
+        /// Konstruktor that starts the server
+        /// </summary>
+        /// <param name="iPort"></param>
         public Host(int iPort)
         {
+            //The Socket which defines the port where it will listen
             var socketHost = new TcpListener(IPAddress.Any, iPort);
             bool runServer = true;
 
+            //While we shall accept incomming requests
             while (runServer)
             {
                 try
                 {
-                    //Get the client
+                    //Accept incomming connection requests
                     var client = socketHost.AcceptTcpClient();
-                    StreamReader a;
+                    //set the streams
+                    StreamReader clientIn = new StreamReader(client.GetStream());
+                    StreamWriter clientOut = new StreamWriter(client.GetStream());
 
+                    //Communicate with client
                     bool clientCommunicate = true;
                     while (clientCommunicate)
                     {
-
+                        //read in the command and process it
+                        string clientCommand = clientIn.ReadLine().Trim();
+                        switch(clientCommand)
+                        {
+                            case "stop server":
+                                runServer = false;
+                                clientCommunicate = false;
+                                break;
+                            case "stop communication":
+                                clientCommunicate = false;
+                                break;
+                            default:
+                                clientOut.WriteLine("Didn't recive valid command!");
+                                break;
+                        }
                     }
                 }
-                catch (Exception)
+                catch(Exception)
                 {
-
-                    throw;
+                    //Log the error or handle it in any other way you want.
                 }
             }
         }
