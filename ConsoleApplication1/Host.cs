b3d24550cff602774c9c2d45c8a9bc29d6b2025f
Noobi - Miles <ltb350@hotmail.de>
2016-11-11 15:19:12 +0100

    inital commit

---
+++ ConsoleApplication1/Host.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Host
+    {
+        public Host(int iPort)
+        {
+            var socketHost = new TcpListener(IPAddress.Any, iPort);
+            bool runServer = true;
+
+            while (runServer)
+            {
+                try
+                {
+                    //Get the client
+                    var client = socketHost.AcceptTcpClient();
+                    StreamReader a;
+
+                    bool clientCommunicate = true;
+                    while (clientCommunicate)
+                    {
+
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+    }
+}
